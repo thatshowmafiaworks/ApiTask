91ca458146ca78865f439ba25ef661fc111a2306
thatshowmafiaworks <shinehd56@gmail.com>
2023-06-19 00:43:17 +0300

    Updated domain entities

--- Domain/Entities/Category.cs
+++ Domain/Entities/Category.cs
@@ -4,6 +4,6 @@
     {
         public string Name { get; set; }
         public string Description { get; set; }
-
+        public List<Product> Products { get; set; } = new List<Product>();
     }
 }
--- Domain/Entities/Product.cs
+++ Domain/Entities/Product.cs
@@ -1,12 +1,15 @@
-﻿namespace Domain.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
 {
     public class Product : BaseEntity
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        public int? CategoryId { get; set; }
         public Category? Category { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
-        public IList<ProductAttribute>? Attributes { get; set; }
+        public List<ProductAttribute> Attributes { get; set; } = new List<ProductAttribute>();
     }
 }
