6d29770b6e657916605740d995864c3d985e7fa9
thatshowmafiaworks <shinehd56@gmail.com>
2023-06-18 23:03:29 +0300

    Added DTO`s

---
+++ Persistence/DTOs/CategoryDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Persistence.DTOs
+{
+    public class CategoryDTO
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
---
+++ Persistence/DTOs/ProductDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Persistence.DTOs
+{
+    public class ProductDTO
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
