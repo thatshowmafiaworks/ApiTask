01510ef1ba73cf3c0cde938bcc5a29b74c66524a
thatshowmafiaworks <shinehd56@gmail.com>
2023-06-25 00:53:35 +0300

    added repositories and MediatR to Program.cs

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,4 +1,13 @@
+using Persistence;
+using Persistence.Repositories;
+using Persistence.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IAttributeRepository, AttributeRepository>();
 
 
 builder.Services.AddControllers();
@@ -6,7 +15,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
